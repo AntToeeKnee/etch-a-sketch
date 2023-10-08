17f9aaf31f7f19b3477c6bdcb85b3df236bc5868
AntToeeKnee <124924118+AntToeeKnee@users.noreply.github.com>
2023-10-08 00:02:27 -0700

    Add hover effect for div elements

--- js-file.js
+++ js-file.js
@@ -1,12 +1,17 @@
 const container = document.querySelector('#container');
 
 //recreating square over and over
-let square; 
+//NTS: Okay so when we did 'let square' on the outside rather than 'const square' on the inside of the for loop, when we did even listener, it
+//was constantly referencing the same square. So rather than giving each div it's own event listener, once we hovered over any square it triggered it.
+//And since they were referencing the same square only the div at the end had it
 for(i = 0; i < 256; i++) {
     //const square is inside to generate a new div
-    square = document.createElement('div');
+    const square = document.createElement('div');
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    });
     square.classList.add('square');
     container.appendChild(square);
 }
 
-container.style.cssText = 'display: flex; text-align:center; height: 50vh; width: 50vw; flex-wrap: wrap; border: 3px solid blue;'
+container.style.cssText = 'display: flex; text-align:center; height: 50vh; width: 35vw; flex-wrap: wrap; border: 3px solid blue;'
