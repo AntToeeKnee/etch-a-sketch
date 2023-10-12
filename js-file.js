3c2b28b5ae2b8313a45ca35af2e78b29711dc21e
AntToeeKnee <124924118+AntToeeKnee@users.noreply.github.com>
2023-10-12 12:58:41 -0700

    Add reset button and text field to change grid size

--- js-file.js
+++ js-file.js
@@ -4,14 +4,39 @@ const container = document.querySelector('#container');
 //NTS: Okay so when we did 'let square' on the outside rather than 'const square' on the inside of the for loop, when we did even listener, it
 //was constantly referencing the same square. So rather than giving each div it's own event listener, once we hovered over any square it triggered it.
 //And since they were referencing the same square only the div at the end had it
-for(i = 0; i < 256; i++) {
-    //const square is inside to generate a new div
-    const square = document.createElement('div');
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    });
-    square.classList.add('square');
-    container.appendChild(square);
+
+let gridCount = 16;
+
+function makeGrid() {
+    const cssSpacing = (gridCount/(gridCount*gridCount)) * 100;
+    const cssString = cssSpacing.toString() + '%';
+    if(gridCount > 64) {
+        alert('Please choose a lower amount for the grid');
+        gridCount = 16;
+        makeGrid();
+    }
+    else { 
+        for(i=0; i < gridCount * gridCount; i++) {
+            const square = document.createElement('div');
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black';
+            });
+            square.classList.add('square');
+            container.appendChild(square);
+            square.style.cssText = 'flex: 0 0 ' + cssString;
+        }
+    }
 }
 
-container.style.cssText = 'display: flex; text-align:center; height: 50vh; width: 35vw; flex-wrap: wrap; border: 3px solid blue;'
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let newGridSize =  document.getElementById("new-size").value;
+    gridCount = newGridSize;
+    makeGrid();
+}
+
+makeGrid();
+
+container.style.cssText = 'display: flex; text-align:center; height: 50vh; width: 28vw; flex-wrap: wrap; border: 3px solid blue;'
